ca77a0feac4c000682fe8280e4d03a627cdcd000
MEBotto <marianobotto92@gmail.com>
2024-06-18 17:12:55 -0300

    build: Se agrego tailwind css para los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-      <h1>Vite + React</h1>
-  )
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
 
-export default App
+export default App;
