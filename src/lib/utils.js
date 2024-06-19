22c8abf74bfe6601c0fcb08355289671e06d2a62
MEBotto <marianobotto92@gmail.com>
2024-06-19 13:21:25 -0300

    feat: Se agrego un archivo utils.js con funciones para generar paginacion y mostrar el toast

---
+++ src/lib/utils.js
@@ -0,0 +1,53 @@
+import { toast, Bounce } from "react-toastify";
+
+export function generatePagination(currentPage, totalPages) {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage <= 4) {
+    return [1, 2, 3, 4, "...", totalPages - 1, totalPages];
+  }
+
+  if (currentPage >= totalPages - 3) {
+    return [
+      1,
+      2,
+      " ...",
+      totalPages - 3,
+      totalPages - 2,
+      totalPages - 1,
+      totalPages,
+    ];
+  }
+
+  return [
+    1,
+    "...",
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    " ...",
+    totalPages,
+  ];
+}
+
+export function showToast(type, message, theme) {
+  const toastOptions = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: theme === "dark" ? "light" : "dark",
+    transition: Bounce,
+  };
+
+  if (type === "success") {
+    toast.success(message, toastOptions);
+  } else if (type === "error") {
+    toast.error(message, toastOptions);
+  }
+}
